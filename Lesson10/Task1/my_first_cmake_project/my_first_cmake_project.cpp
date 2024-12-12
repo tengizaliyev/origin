dbaf46af668387c247bc6d7f6e808843754cbdd9
Tengiz Aliyev <tengiz.buisness@gmail.com>
2024-12-13 00:15:07 +0400

    Добавляю первое задание по CMake

---
+++ Lesson10/Task1/my_first_cmake_project/my_first_cmake_project.cpp
@@ -0,0 +1,17 @@
+﻿#include <iostream>
+#include <string>
+#include <windows.h>
+
+int main() {
+    // Настройка вывода для поддержки русских букв
+    SetConsoleOutputCP(1251);
+    SetConsoleCP(1251);
+
+    std::string name;
+    std::cout << "Введите имя: ";
+    std::cin >> name;
+
+    std::cout << "Здравствуйте, " << name << "!" << std::endl;
+
+    return 0;
+}
